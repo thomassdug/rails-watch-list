0590ea5d3b37eb631e1df805705703170dfb84e4
Thomas Dugue <thomas.dugue@edhec.com>
2021-06-10 17:13:37 +0200

    added some content

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
+  root to: 'pages#home'
 end
